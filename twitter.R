398d4fa6aa24b7d81414a705f02019c3c15906d3
skylerg022 <skylerg022@gmail.com>
2020-05-01 14:15:41 -0600

    Adding group code file and trivial submission

---
+++ twitter.R
@@ -0,0 +1,46 @@
+###########
+# Twitter #
+###########
+
+library(tidyverse)
+
+twitter <- read_csv("train.csv")
+
+### Libraries ###
+library(stringr)
+
+# Specific words within text
+# #, urls, including images
+# length of texts
+# # number of capital letters, exlamation marks, question marks
+# Tone of the message - along with key words
+# emojis
+
+#1 Punctuation - .!?,"'-
+twitter$url_count <- str_count(twitter$text, "http(.*)$")
+
+# Removed urls
+twitter$text <- gsub("http(.*)$", "http", twitter$text)
+
+twitter$punct_count <- str_count(twitter$text, "[.!?,\"'-]" )
+
+#2 Handles - @
+twitter$handles_count <- str_count(twitter$text, "[@]" )
+
+#3 Hashtag - #
+twitter$hashtag_count <- str_count(twitter$text, "[#]" )
+
+#4 Length
+twitter$characters <- nchar(twitter$text)
+
+# Capital letter
+twitter$capital <- str_count(twitter$text, "[A-Z]")
+head(twitter)
+
+# Numbers
+twitter$numbers <- str_count(twitter$text, "[0-9]")
+
+# Naive Bayes: David 
+# SVM: Matt
+# Random Forest: Shane
+# KNN: McKay
