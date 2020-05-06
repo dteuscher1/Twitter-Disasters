34c0f9464ed03cea65a0be0434379422f484e03e
skylerg022 <skylerg022@gmail.com>
2020-05-06 14:36:28 -0600

    Prediction code added

--- td-idf.R
+++ td-idf.R
@@ -34,7 +34,7 @@ it_train <- itoken(twitter$text,
                   tokenizer = tok_fun, 
                   ids = twitter$id, 
                   progressbar = TRUE)
-vocab <- create_vocabulary(it_train)
+vocab <- create_vocabulary(it_train, c(1L, 1L))
 
 vectorizer <- vocab_vectorizer(vocab)
 dtm_train <- create_dtm(it_train, vectorizer)
@@ -54,3 +54,24 @@ print(difftime(Sys.time(), t1, units = 'sec'))
 
 plot(glmnet_classifier)
 print(paste("max AUC =", round(max(glmnet_classifier$cvm), 4)))
+
+
+lambda_hat <- glmnet_classifier$lambda.1se
+
+twitter_test <- read_csv('test.csv')
+it_test <- itoken(twitter_test$text, 
+                   preprocessor = prep_fun, 
+                   tokenizer = tok_fun, 
+                   ids = twitter_test$id, 
+                   progressbar = TRUE)
+vocab <- create_vocabulary(it_test, c(1L, 2L))
+
+dtm_test <- create_dtm(it_test, vectorizer)
+dtm_test_tfidf <- transform(dtm_test, tfidf)
+
+model <- glmnet(x = dtm_train_tfidf, y = twitter$target, 
+                family = 'binomial', lambda=lambda_hat)
+y_hat <- predict(model, dtm_test_tfidf)
+y_hat <- as.integer(y_hat > 0)
+submission_word2vec <- tibble(id=twitter_test$id, target=y_hat)
+write_csv(submission_word2vec, 'submission_word2vec.csv')
