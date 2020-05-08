5c000262185815b26a9d397c2303172fe3ee17fa
dteuscher1 <dteuscher.37.12@gmail.com>
2020-05-08 13:39:29 -0600

    Adjustments to the linear classifier and submission file

--- td-idf.R
+++ td-idf.R
@@ -3,6 +3,8 @@ library(tidyverse)
 library(text2vec)
 
 twitter <- read_csv('train.csv')
+twitter_test <- read_csv('test.csv')
+
 
 tweets <- twitter %>% 
   unnest_tokens(word, text) %>% 
@@ -34,6 +36,11 @@ it_train <- itoken(twitter$text,
                   tokenizer = tok_fun, 
                   ids = twitter$id, 
                   progressbar = TRUE)
+it_test <- itoken(twitter_test$text,
+                  preprocessor = prep_fun,
+                  tokenizer = tok_fun,
+                  ids = twitter_test$id,
+                  progressbar = TRUE)
 vocab <- create_vocabulary(it_train, c(1L, 1L))
 
 vectorizer <- vocab_vectorizer(vocab)
@@ -41,21 +48,28 @@ dtm_train <- create_dtm(it_train, vectorizer)
 tfidf <- TfIdf$new()
 dtm_train_tfidf <- fit_transform(dtm_train, tfidf)
 
+dtm_test_tfidf = create_dtm(it_test, vectorizer)
+dtm_test_tfidf = transform(dtm_test_tfidf, tfidf)
 t1 <- Sys.time()
 library(glmnet)
 glmnet_classifier <- cv.glmnet(x = dtm_train_tfidf, y = twitter$target, 
                               family = 'binomial', 
                               alpha = 1,
                               type.measure = "auc",
-                              nfolds = 4,
-                              thresh = 1e-3,
-                              maxit = 1e3)
+                              nfolds = 8,
+                              thresh = 0.0001,
+                              maxit = 10000)
 print(difftime(Sys.time(), t1, units = 'sec'))
 
 plot(glmnet_classifier)
 print(paste("max AUC =", round(max(glmnet_classifier$cvm), 4)))
 
+preds <- predict(glmnet_classifier, dtm_test_tfidf, type = "class")
+submission <- tibble(id=twitter_test$id, target=as.numeric(preds))
 
+
+
+## Skyler's attempt
 lambda_hat <- glmnet_classifier$lambda.1se
 
 twitter_test <- read_csv('test.csv')
